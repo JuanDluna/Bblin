8ad4eb97a2f12050f010d13430dc93ffb7ff8426
Juan De Luna <pablo.dluna@hotmail.com>
2023-06-19 13:44:54 -0600

    Validaciones
    *Si es Ncurses* 
    Realiza validacion si se introduce un archivo por parametros, si no se introducio ninguno o si se introducio mas de 1

--- Bblin.sh
+++ Bblin.sh
@@ -1,112 +1,56 @@
 #!/bin/bash
 
-# Importar la biblioteca ncurses
-source /usr/share/ncurses/ncurses.sh
+# Función para mostrar el "File chooser"
+mostrar_file_chooser() {
+    echo "Por favor, ingresa la ruta del archivo que deseas editar:"
+    read -e archivo
 
-# Función para inicializar la interfaz de usuario
-inicializar_interfaz() {
-    # Inicializar ncurses
-    tput init
-}
-
-# Función para finalizar la interfaz de usuario
-finalizar_interfaz() {
-    # Finalizar ncurses
-    tput reset
-}
-
-# Función para mostrar la interfaz principal del editor
-mostrar_interfaz_principal() {
-    # Lógica para mostrar la interfaz principal del editor
-    clear
-    tput bold
-    tput cup 5 10
-    echo "Bienvenido al editor de texto"
-    tput sgr0
-    tput cup 8 10
-    echo "Presiona una tecla para continuar..."
-    read -n 1
-}
-
-# Función para mostrar el menú de opciones
-mostrar_menu() {
-    # Lógica para mostrar el menú de opciones
-    clear
-    tput bold
-    tput cup 5 10
-    echo "Menú de opciones"
-    tput sgr0
-    tput cup 7 10
-    echo "1. Editar archivo"
-    tput cup 8 10
-    echo "2. Buscar cadena"
-    tput cup 9 10
-    echo "3. Salir"
-    tput cup 11 10
-    echo "Elige una opción: "
-    read opcion
-}
-
-# Función para procesar la opción elegida en el menú
-procesar_opcion() {
-    case $opcion in
-        1)
-            editar_archivo
-            ;;
-        2)
-            buscar_cadena
-            ;;
-        3)
-            salir
-            ;;
-        *)
-            mostrar_error "Opción inválida"
-            ;;
-    esac
+    # Verificar si el archivo existe
+    if test -f "$archivo"; then
+        echo "Archivo encontrado: $archivo"
+    else
+        echo "El archivo no existe."
+        exit 1
+    fi
 }
 
 # Función para editar el archivo
 editar_archivo() {
-    # Lógica para la edición del archivo
-    echo "Editando archivo..."
-}
-
-# Función para buscar una cadena en el archivo
-buscar_cadena() {
-    # Lógica para buscar una cadena en el archivo
-    echo "Buscando cadena..."
-}
-
-# Función para salir del programa
-salir() {
-    # Finalizar la interfaz y salir del programa
-    finalizar_interfaz
-    exit 0
-}
-
-# Función para mostrar un mensaje de error
-mostrar_error() {
-    mensaje=$1
-    tput bold
-    tput setaf 1
-    tput cup 13 10
-    echo "Error: $mensaje"
-    tput sgr0
-    tput cup 15 10
-    echo "Presiona una tecla para continuar..."
-    read -n 1
+    echo "Editando archivo: $archivo"
+    # Lógica de edición del archivo
 }
 
 # Función principal
 main() {
-    inicializar_interfaz
-    mostrar_interfaz_principal
+    # Verificar el número de parámetros
+    if [[ $# -gt 1 ]]; then
+        echo "Error: Demasiados parámetros. Se espera solo un archivo como parámetro."
+        exit 1
+    fi
 
-    while true; do
-        mostrar_menu
-        procesar_opcion
-    done
+    # Verificar si se pasó un archivo por parámetro
+    if [[ -n $1 ]]; then
+        archivo="$1"
+        # Verificar si el archivo existe
+        if test -f "$archivo"; then
+            echo "Archivo encontrado: $archivo"
+        else
+            echo "El archivo no existe."
+            exit 1
+        fi
+    else
+        mostrar_file_chooser
+    fi
+
+    editar_archivo
+    # Resto de la lógica de tu programa
 }
 
+# Validar el número de parámetros antes de llamar a main
+if [[ $# -gt 1 ]]; then
+    echo "Error: Demasiados parámetros. Se espera solo un archivo como parámetro."
+    exit 1
+fi
+
 # Llamada a la función principal
-main
+main "$@"
